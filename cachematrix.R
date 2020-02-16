394b914f5967084eb7943f774405ac43bf62ec41
SunPhat <nattanan.pha@gmail.com>
2020-02-16 14:02:37 +0700

    This is my completed code for submitting as 2nd programing assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+#In my code, "matlib" package is used to calculate inverse matrix by calling Inv().
+#In makeCacheMatrix function, I try to set the value of the matrix and get the value of the matrix
+#after that, I set the value of inverse matrix and get the inverse matrix value.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(Inv) m <<- Inv
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+#cacheSolve function is used to check that if the inverse matrix has already been calculated.
+#If so, the function will return the value from catched data. 
+#Otherwise, Inverse matrix need to be computed and the new value will be returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("Yeah! there are the value in cached data so the inverse matrix don't need to be computed")
+    return(m)  #return the value of the inverse of 'x' from catched data
+  }
+  data <- x$get()
+  m <- inv(data, ...)
+  x$setInverse(m)
+  m #return new value of the inverse of 'x' from computation
 }
